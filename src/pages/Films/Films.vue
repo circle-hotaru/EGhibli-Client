2829649a00a24f03ff5921c2217acff4a87adb7f
circlehotaru <mightyherox@gmail.com>
2019-12-19 22:21:12 +0800

    add search page

--- src/pages/Films/Films.vue
+++ src/pages/Films/Films.vue
@@ -11,6 +11,7 @@
 
 <script>
 import Film from "../../components/Film";
+import bus from "../../eventBus";
 export default {
   name: "Films",
   data() {
@@ -25,7 +26,7 @@ export default {
         },
         {
           cover: "../../../public/images/test2.png",
-          name: "哈尔的移动城堡",
+          title: "哈尔的移动城堡",
           director: "宫崎骏",
           description:
             "继母因无力负担生活，将苏菲和她的两个妹妹都送到了制帽店去当学徒。两个妹妹很快先后就离开了制帽店去追寻各自的梦想，只有苏菲坚持了下来。一天，小镇旁边来了一座移动堡垒，传说堡垒的主人哈尔专吸取年青姑娘的灵魂，所以小镇的姑娘都不敢靠近。"
@@ -39,7 +40,7 @@ export default {
         },
         {
           cover: "../../../public/images/test2.png",
-          name: "哈尔的移动城堡",
+          title: "哈尔的移动城堡",
           director: "宫崎骏",
           description:
             "继母因无力负担生活，将苏菲和她的两个妹妹都送到了制帽店去当学徒。两个妹妹很快先后就离开了制帽店去追寻各自的梦想，只有苏菲坚持了下来。一天，小镇旁边来了一座移动堡垒，传说堡垒的主人哈尔专吸取年青姑娘的灵魂，所以小镇的姑娘都不敢靠近。"
@@ -53,7 +54,7 @@ export default {
         },
         {
           cover: "../../../public/images/test2.png",
-          name: "哈尔的移动城堡",
+          title: "哈尔的移动城堡",
           director: "宫崎骏",
           description:
             "继母因无力负担生活，将苏菲和她的两个妹妹都送到了制帽店去当学徒。两个妹妹很快先后就离开了制帽店去追寻各自的梦想，只有苏菲坚持了下来。一天，小镇旁边来了一座移动堡垒，传说堡垒的主人哈尔专吸取年青姑娘的灵魂，所以小镇的姑娘都不敢靠近。"
